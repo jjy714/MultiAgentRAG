c0100d8115258464987d7bced81f7502350d764b
Jason Jung <jjy714@gmail.com>
2025-10-12 02:24:16 +0900

    Initial Commit
    Signed-off-by: Jason Jung <jjy714@gmail.com>

---
+++ agents/Agent.py
@@ -0,0 +1,11 @@
+from abc import ABC
+import BaseModule
+
+class Agent(ABC):
+    
+    def __init__():
+        pass
+    
+    @abstractmethod
+    def communicate(self):
+        pass
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from multiagentrag!")
+
+
+if __name__ == "__main__":
+    main()
